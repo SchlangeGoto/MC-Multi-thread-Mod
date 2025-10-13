4424b9decde5b2113e37a524f48a83f9108f739c
SchlangeGoto <165955818+SchlangeGoto@users.noreply.github.com>
2025-10-13 23:23:24 +0200

    First commit

---
+++ src/client/java/dev/kkazi/mcMultiThreadFabric/client/McMultiThreadFabricClient.java
@@ -0,0 +1,10 @@
+package dev.kkazi.mcMultiThreadFabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McMultiThreadFabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/kkazi/mcMultiThreadFabric/McMultiThreadFabric.java
@@ -0,0 +1,10 @@
+package dev.kkazi.mcMultiThreadFabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class McMultiThreadFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
