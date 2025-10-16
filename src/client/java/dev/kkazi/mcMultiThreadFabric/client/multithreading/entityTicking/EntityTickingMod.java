1c89825498f6c91c240a6ae701f3a0a61177956a
SchlangeGoto <165955818+SchlangeGoto@users.noreply.github.com>
2025-10-16 23:29:14 +0200

    Made mixin configs right, added first prototype of client side entity tick multithreading

--- src/client/java/dev/kkazi/mcMultiThreadFabric/client/multithreading/entityTicking/EntityTickingMod.java
+++ src/client/java/dev/kkazi/mcMultiThreadFabric/client/multithreading/entityTicking/EntityTickingMod.java
@@ -1,10 +1,36 @@
 package dev.kkazi.mcMultiThreadFabric.client.multithreading.entityTicking;
 
-import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.EntityList;
+import net.minecraft.world.tick.TickManager;
+import org.spongepowered.asm.mixin.*;
 
-import javax.swing.text.html.parser.Entity;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-@Mixin (Entity.class)
-public class EntityTickingMod {
+@Mixin (ClientWorld.class)
+public abstract class EntityTickingMod {
+    @Shadow @Final private EntityList entityList;
+
+    @Shadow @Final private TickManager tickManager;
+
+    @Shadow public abstract void tickEntity(Entity entity);
+
+    ExecutorService pool = Executors.newWorkStealingPool();
+
+    @Overwrite
+    public void tickEntities() {
+        System.err.println("Its gets called");
+        this.entityList.forEach(entity -> {
+            if (entity.isRemoved() || entity.hasVehicle() || this.tickManager.shouldSkipTick(entity)) {
+                return;
+            }
+            pool.submit(() -> {
+                this.tickEntity((Entity) entity);
+            });
+        });
+     }
 
 }
